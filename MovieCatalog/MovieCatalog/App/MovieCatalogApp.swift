56605dadf5611489f52840a8f44878d61a95332a
Felipe Espinoza <fespinozacast@gmail.com>
2025-02-07 21:24:06 +0100

    Setup project

---
+++ MovieCatalog/MovieCatalog/App/MovieCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCatalogApp.swift
+//  MovieCatalog
+//
+//  Created by Felipe Espinoza on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
