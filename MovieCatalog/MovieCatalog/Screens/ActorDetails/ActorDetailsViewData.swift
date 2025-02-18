0e9390c3b54af20d46bf33f925119b7d59f1331f
Felipe Espinoza <fespinozacast@gmail.com>
2025-02-18 22:39:18 +0100

    Add pedro pascal as sample actor

--- MovieCatalog/MovieCatalog/Screens/ActorDetails/ActorDetailsViewData.swift
+++ MovieCatalog/MovieCatalog/Screens/ActorDetails/ActorDetailsViewData.swift
@@ -24,10 +24,15 @@ extension ActorDetailsViewData {
     static func previewValue(
         id: ActorID = .randomPreviewId(),
         name: String = "Pedro Pascal",
-        profilePicture: ImageViewData = .image(Image(.Actor.BradPitt.medium)),
+        profilePicture: ImageViewData = .image(Image(.Actor.PedroPascal.medium)),
         bio: String = """
-        A young Donald Trump, eager to make his name as a hungry scion of a wealthy family in 1970s New York, \
-        comes under the spell..
+        José Pedro Balmaceda Pascal (born April 2, 1975) is a Chilean and American actor. After nearly two decades \
+        of taking minor roles on stage and television, Pascal had his breakout role as Oberyn Martell in the fourth \
+        season of the HBO fantasy series Game of Thrones (2014). He gained further prominence with his portrayal of \
+        Javier Peña in the Netflix crime series Narcos (2015–2017). He went on to appear in the films The Great Wall \
+        (2016), Kingsman: The Golden Circle (2017), The Equalizer 2 (2018), and Triple Frontier (2019).\n\nPascal's \
+        leading roles as Din Djarin in the Disney+ science fiction series The Mandalorian (2019–2023) and Joel Miller \
+        in the HBO post-apocalyptic drama series The Last of Us (2023–present)...
         """,
         movies: [MovieCardViewData] = [
             .gladiatorTwo(),
