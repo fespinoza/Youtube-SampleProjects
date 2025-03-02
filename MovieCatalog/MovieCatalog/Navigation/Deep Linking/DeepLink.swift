2ba7d43e5e5a55cf9658272075243a9a307653f4
Felipe Espinoza <fespinozacast@gmail.com>
2025-03-02 13:20:35 +0100

    App deep link support and mapping to destinations

---
+++ MovieCatalog/MovieCatalog/Navigation/Deep Linking/DeepLink.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+struct DeepLink {
+    static let scheme: String = "moviecat"
+
+    static func destination(from url: URL) -> Destination? {
+        guard url.scheme == DeepLink.scheme else { return nil }
+
+        for parser in registeredParsers {
+            if let destination = parser.parse(url) {
+                return destination
+            }
+        }
+
+        return nil
+    }
+
+    static let registeredParsers: [DeepLinkParser] = [
+        .equal(to: ["home"], destination: .tab(.home)),
+        .equal(to: ["search"], destination: .tab(.search)),
+        .equal(to: ["release-calendar"], destination: .tab(.releaseCalendar)),
+        .equal(to: ["favorites"], destination: .tab(.favorites)),
+
+        .equal(to: ["list", "upcoming"], destination: .push(.movieList(.upcoming))),
+        .equal(to: ["list", "top-rated"], destination: .push(.movieList(.topRated))),
+        .equal(to: ["list", "popular"], destination: .push(.movieList(.popular))),
+
+        .movieDetails,
+        .movieDetailsDescription,
+        .movieDetailsGallery,
+
+        .actorDetails,
+    ]
+}
