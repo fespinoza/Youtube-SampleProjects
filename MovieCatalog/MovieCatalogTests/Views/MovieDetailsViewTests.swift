a1ee36e9a7ecf859d2d8cfa34113f430fe0d6ff9
Felipe Espinoza <fespinozacast@gmail.com>
2025-03-02 13:19:29 +0100

    Update tests

--- MovieCatalog/MovieCatalogTests/Views/MovieDetailsViewTests.swift
+++ MovieCatalog/MovieCatalogTests/Views/MovieDetailsViewTests.swift
@@ -9,6 +9,7 @@ struct MovieDetailsViewTests {
     @Test("Movie with all data", arguments: SnapshotVariant.defaultVariants())
     func movieDetails(_ variant: SnapshotVariant) async throws {
         let view = NavigationStack { MovieDetailsView(viewData: .previewValue()) }
+            .environment(Router.previewRouter())
 
         expectSnapshot(of: view, on: variant)
     }
@@ -16,6 +17,7 @@ struct MovieDetailsViewTests {
     @Test("Movie that doens't have a gallery", arguments: SnapshotVariant.fixedHeightVariants())
     func movieDetailsNoGallery(_ variant: SnapshotVariant) async throws {
         let view = NavigationStack { MovieDetailsView(viewData: .previewValue(galleryItems: [])) }
+            .environment(Router.previewRouter())
 
         expectSnapshot(of: view, on: variant)
     }
@@ -23,6 +25,7 @@ struct MovieDetailsViewTests {
     @Test("Movie that doens't have a actors", arguments: SnapshotVariant.fixedHeightVariants())
     func movieDetailsNoActors(_ variant: SnapshotVariant) async throws {
         let view = NavigationStack { MovieDetailsView(viewData: .previewValue(actors: [])) }
+            .environment(Router.previewRouter())
 
         expectSnapshot(of: view, on: variant)
     }
@@ -30,6 +33,7 @@ struct MovieDetailsViewTests {
     @Test("Movie missing a poster image", arguments: SnapshotVariant.minimalVariants())
     func movieDetailsMissingPoster(_ variant: SnapshotVariant) async throws {
         let view = NavigationStack { MovieDetailsView(viewData: .previewValue(poster: nil)) }
+            .environment(Router.previewRouter())
 
         expectSnapshot(of: view, on: variant)
     }
@@ -37,6 +41,7 @@ struct MovieDetailsViewTests {
     @Test("Movie with all data - large size")
     func movieDetailsAccessbility() {
         let view = NavigationStack { MovieDetailsView(viewData: .previewValue()) }
+            .environment(Router.previewRouter())
 
         expectSnapshot(
             of: view,
