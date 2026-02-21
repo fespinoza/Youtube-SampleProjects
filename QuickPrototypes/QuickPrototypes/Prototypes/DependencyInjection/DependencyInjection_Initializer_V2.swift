ab6f7bddab0803f41950b35ed545f1af2b49b9a4
Felipe Espinoza <fespinozacast@gmail.com>
2026-02-21 20:21:22 +0100

    WIP

--- QuickPrototypes/QuickPrototypes/Prototypes/DependencyInjection/DependencyInjection_Initializer_V2.swift
+++ QuickPrototypes/QuickPrototypes/Prototypes/DependencyInjection/DependencyInjection_Initializer_V2.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftUI
 import Testing
 
-enum DependencyInjection_Initializer_V1 {
+enum DependencyInjection_Initializer_V2 {
     struct Post: Decodable {
         let title: String
         let body: String
@@ -18,14 +18,18 @@ enum DependencyInjection_Initializer_V1 {
     @Observable
     class PostListViewModel {
         var posts: [Post]
+        let fetchPosts: () async throws -> [Post]
 
-        init() {
+        init(
+            fetchPosts: @escaping () async throws -> [Post] = HttpClient.fetchPosts
+        ) {
             self.posts = []
+            self.fetchPosts = fetchPosts
         }
 
         func loadData() async {
             do {
-                posts = try await HttpClient.fetchPosts()
+                posts = try await fetchPosts()
             } catch {
                 print(error.localizedDescription)
             }
@@ -37,11 +41,18 @@ enum DependencyInjection_Initializer_V1 {
 //    struct PostListViewModelTests {
 //        @Test
 //        func `the view model populates post from a network request to our backend`() async {
-//            let viewModel = PostListViewModel()
+//            let viewModel = PostListViewModel(
+//                fetchPosts: {
+//                    [
+//                        Post(title: "Hello World!", body: "..."),
+//                        Post(title: "Second Post", body: "..."),
+//                        Post(title: "Third Post", body: "..."),
+//                    ]
+//                })
 //
 //            await viewModel.loadData()
 //
-//            #expect(viewModel.posts.count != 0, "We should have gotten posts")
+//            #expect(viewModel.posts.count == 3, "We should have gotten posts")
 //        }
 //    }
 }
